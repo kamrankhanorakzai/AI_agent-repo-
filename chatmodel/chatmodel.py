bc2209ae185104d8b7d6be16be17b2667a6f02fe
kamrankhanorakzai <turababu980@gmail.com>
2026-02-20 05:55:55 +0530

    structure_output-parsers

--- chatmodel/chatmodel.py
+++ chatmodel/chatmodel.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-from langchain.schema import HumanMessage, SystemMessage
+# from langchain.schema import HumanMessage, SystemMessage
 
-chatmodel=ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.9)
+# chatmodel=ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.9)
 
-messages = [
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content="Explain Docker simply")
-]
+# messages = [
+#     SystemMessage(content="You are a helpful assistant."),
+#     HumanMessage(content="Explain Docker simply")
+# ]
 
-response = chatmodel.generate(messages)
-print(response.generations[0][0].text)
+# response = chatmodel.generate(messages)
+# print(response.generations[0][0].text)
 
-llm=HuggingFaceEndpoint(repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",task="text-generation")
+llm=HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V3.2",task="text-generation")
 
 chat=ChatHuggingFace(llm=llm,)
 print(chat.invoke("What is the capital of France?"))
